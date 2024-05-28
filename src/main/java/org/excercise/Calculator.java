d3a8cc6b7b13e6bdc194d85d209a1dd9ceb45eea
AimanFadil <aimanbello2001@gmail.com>
2024-05-28 14:31:35 +0200

    Calculator

--- src/main/java/org/excercise/Calculator.java
+++ src/main/java/org/excercise/Calculator.java
@@ -1,4 +1,24 @@
 package org.excercise;
 
 public class Calculator {
+    public float add(float num1, float num2){
+        float sum = num1 + num2;
+        return sum;
+    }
+
+    public float substract(float num1, float num2){
+        float sub = num1 - num2;
+        return sub;
+    }
+
+    public float divide(float num1, float num2){
+        float div = num1 / num2;
+        return div;
+    }
+
+    public float multiply(float num1, float num2){
+        float mult = num1 * num2;
+        return mult;
+    }
+
 }
